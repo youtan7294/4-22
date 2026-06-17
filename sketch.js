5fcc0c47d8c366f35abf4d074e64acae76b2354e
youtan7294 <sshuku221108@shukutoku.ed.jp>
2026-06-17 09:17:14 +0000

    papa

--- sketch.js
+++ sketch.js
@@ -1,9 +1,26 @@
+let x;
+
 function setup() {
-    createCanvas(1000, 1000);
+  createCanvas(1000, 1000);
+  x=0;
 }
 
 function draw(){
-    background(220);
+    background(400);
     circle(width/2, height/2,50
     );
+
+    x=x+20;
+    if(x>width){
+       x=0;
+    }
+circle(x,250,50)
+textSize(100);
+textAlign(CENTER,CENTER)
+text("よろしく",500,500)
+
+
+
+
+
 }
